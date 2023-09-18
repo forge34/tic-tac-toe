1970d470c858c836fd8ec72ee6a7ffa7ba14455d
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2023-09-19 00:50:19 +0300

    basic working game

--- script.js
+++ script.js
@@ -4,23 +4,47 @@ const O = 'svg/O.svg'
 
 
 
-const Game = (function(){
+const Game = (function () {
     let g = document.getElementById('game-board')
     let game_board = new Array(9)
+    let marker = ''
 
-    let combination =[[0,1,2],[3,4,5],[6,7,8],[1,4,7],[0,3,6],[2,5,8],[6,4,2],[0,4,8]]
+    let combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [1, 4, 7], [0, 3, 6], [2, 5, 8], [6, 4, 2], [0, 4, 8]]
 
-    function checker(element ) {
-        return game_board[element] === 'X'
+    function set_turn() {
+        if (marker === 'X') {
+            marker = 'O'
+        }
+        else if (marker === 'O') { marker = 'X' }
+    }
+
+    function clear_grid() {
+
+        for (let i = 0; i < game_board.length; i++) {
+            delete game_board[i]
+
+        }
+
+        let div = g.querySelectorAll('div')
+
+        div.forEach((element) => {
+            if (element.firstChild) {
+                element.removeChild(element.firstChild)
+            }
+        })
+    }
+
+    function checker(element) {
+        return game_board[element] === marker
     }
 
     function play() {
-       combination.forEach((element) => {
-        if (element.every(checker))
-        {
-            alert("Win")
-        }
-    })
+        combination.forEach((element) => {
+            if (element.every(checker)) {
+                alert(`${marker} Win`)
+                clear_grid()
+            }
+        })
     }
 
     const create_grid = () => {
@@ -28,61 +52,59 @@ const Game = (function(){
             let div = document.createElement('div')
 
             div.classList.add('cell')
-            div.dataset.id= i
+            div.dataset.id = i
 
             g.append(div)
 
         }
     }
 
-    const add_marker = (m , element) => {
-        
-        play()
-        if (element.querySelector('img') != null){
+    function add_marker(element) {
+
+        if (element.querySelector('img') != null) {
             return false
         }
 
-        game_board[element.dataset.id] = m
+        game_board[element.dataset.id] = marker
         let img = document.createElement('img')
 
-        if (m === 'X') {
+        if (marker === 'X') {
             img.src = X
         } else {
             img.src = O
         }
 
         element.append(img)
+        set_turn()
+        play()
     }
 
 
-    const cell_click = (p) => {
+    function cell_click(m) {
         let cells = document.querySelectorAll('.cell')
+        marker = m
 
         for (let i = 0; i < cells.length; i++) {
             const element = cells[i];
 
-            element.addEventListener('click' , () => {add_marker(p , element)})
-            
+            element.addEventListener('click', () => { add_marker(element) })
+
         }
     }
 
     return {
-        create_grid , 
+        create_grid,
         click: (p) => cell_click(p)
     }
 })()
 
-function Player(marker){
+function Player(marker) {
     return {
         marker
     }
 }
 
 let p1 = Player('X')
-let p2 = Player('O')
-
 
 Game.create_grid()
 Game.click(p1.marker)
-
-let combination =[[0,1,2],[3,4,5],[6,7,8],[1,4,7],[0,3,6],[2,5,8],[6,4,2],[0,4,8]]
